82c7f623277063e109dd17f090b3c42e9d95c163
Seth Cohen <smcohen82@gmail.com>
2021-02-09 17:58:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,9 +8,10 @@ def nyc_pigeon_organizer(data)
         
         if !new_hash[name]
           new_hash[name] = {}
-          
-        binding.pry
         end
+        
+        if !new_hash[name][k]
+          
       end
     end
   end
