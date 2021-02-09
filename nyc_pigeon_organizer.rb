4f757ab7c478664e9b9986dfaec6bbf6b429e072
Seth Cohen <smcohen82@gmail.com>
2021-02-09 17:49:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |k, v|
     v.each do |inner_key, names|
-      
-      names.each do |name|
       binding.pry
+      names.each do |name|
+      
       new_hash = {name => v}
       
       end
